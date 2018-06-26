dd306463352822e4368977ba0b90681ea7ef5df1
pedrocontreras <pcontreras@summit-tech.ca>
2018-06-26 11:22:28 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Pedro Contreras on 2018-06-26.
+//  Copyright © 2018 Pedro Contreras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
